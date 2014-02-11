9c9fb7f881e813f6528f25f01b289b4fa2fd4c24
Olivier Bazoud <olivier.bazoud@gmail.com>
2014-02-11 16:09:35 +0100

    regexp wip

--- imdb-data/src/main/java/com/bazoud/springbatch/imdb/data/reader/ImdbMovieLineTokenizer.java
+++ imdb-data/src/main/java/com/bazoud/springbatch/imdb/data/reader/ImdbMovieLineTokenizer.java
@@ -7,33 +7,27 @@ import org.springframework.beans.factory.InitializingBean;
 
 public class ImdbMovieLineTokenizer extends RegexLineTokenizer implements InitializingBean {
   private static final Logger LOG = LoggerFactory.getLogger(ImdbMovieLineTokenizer.class);
-  private String DATE_PATTERN = "(\\d{4}||\\?{4})";
-
   private String MOVIE_NAME_PATTERN = "\"(.+?)\"";
 
-  // private String RELEASE_DATE_PATTERN = "\\s\\((?:(?:(\\d{4})||\\?{4})(?:/[IXV]+)?\\)?)\\)";
-  private String RELEASE_DATE_PATTERN = "\\((?:(\\d{4}||\\?{4}))(?:\\/([IXV]+))?\\)";
+  private String RELEASE_DATE_PATTERN = "\\s+\\((?:(\\d{4}||\\?{4}))(?:\\/([IXV]+))?\\)";
 
-  private String TV_PATTERN = "(?:\\(((?:V)||(?:TV)||(?:VG))\\))?";
+  private String TV_PATTERN = "(?:\\s+\\(((?:V)||(?:TV)||(?:VG))\\))?";
+
   private String EPISODE_PATTERN = "(?:\\s+\\{(.+?)?(?:\\s+\\((?:#(?:(\\d+?)\\.(\\d+?)))?(\\d{4}-\\d{2}-\\d{2})?\\))?\\})?";
 
-  //private String BROADCAST_DATE_PATTERN = "(?:\\s*\\t(?:(\\d{4})||\\?{4})(?:-(?:(\\d{4})||\\?{4}))?)";
-  private String BROADCAST_DATE_PATTERN = "(?:(\\d{4}||\\?{4})(?:\\-(?:(\\d{4}||\\?{4})))?)";
+  private String MOVIE_STATE = "(?:\\s+\\{\\{?(.+?)?\\}\\}?)?";
 
-  private String COMMENTARY = "(?:\\s+(.+))?";
+  private String BROADCAST_DATE_PATTERN = "(?:\\s+(\\d{4}||\\?{4})(?:\\-(?:(\\d{4}||\\?{4})))?)";
 
   @Override
   public void afterPropertiesSet() throws Exception {
-    // "Title" (year) (type) {episode title (#season number.episode number)} (year) (comment)
+    // "Title" (release) (type) {episode title (#season number.episode number) (episode date)} (year-year) (comment)
     String pattern = "^"
         + MOVIE_NAME_PATTERN
-        + "\\s+"
         + RELEASE_DATE_PATTERN
-        + "\\s+"
         + TV_PATTERN
-        + "\\s+"
         + EPISODE_PATTERN
-        + "\\s+"
+        + MOVIE_STATE
         + BROADCAST_DATE_PATTERN
         + "$";
     LOG.info(pattern);
--- imdb-data/src/test/java/com/bazoud/springbatch/imdb/data/reader/ImdbMovieLineTokenizerTest.java
+++ imdb-data/src/test/java/com/bazoud/springbatch/imdb/data/reader/ImdbMovieLineTokenizerTest.java
@@ -7,7 +7,7 @@ import org.springframework.batch.item.file.transform.FieldSet;
 import static org.junit.Assert.assertEquals;
 
 public class ImdbMovieLineTokenizerTest {
-  private final int FIELD_COUNT = 10;
+  private final int FIELD_COUNT = 11;
   private ImdbMovieLineTokenizer lineTokenizer;
   private int index = 0;
 
@@ -21,9 +21,9 @@ public class ImdbMovieLineTokenizerTest {
   @Test
   public void test_single_movie() {
     FieldSet fieldSet = lineTokenizer.tokenize("\"#1 Single\" (2006)\t\t\t\t\t2006-????");
-    assertEquals(FIELD_COUNT, fieldSet.getFieldCount());
     assertFiedEquals("#1 Single", fieldSet, index++);
     assertFiedEquals("2006", fieldSet, index++);
+    assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals(null, fieldSet, index++);
@@ -38,13 +38,15 @@ public class ImdbMovieLineTokenizerTest {
   @Test
   public void test_single_movie_episode() {
     FieldSet fieldSet = lineTokenizer.tokenize("\"#1 Single\" (2006) {Cats and Dogs (#1.4)}\t\t\t\t\t2006");
-    assertEquals(FIELD_COUNT, fieldSet.getFieldCount());
     assertFiedEquals("#1 Single", fieldSet, index++);
     assertFiedEquals("2006", fieldSet, index++);
+    assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals("Cats and Dogs", fieldSet, index++);
     assertFiedEquals("1", fieldSet, index++);
     assertFiedEquals("4", fieldSet, index++);
+    assertFiedEquals(null, fieldSet, index++);
+    assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals("2006", fieldSet, index++);
     assertFiedEquals(null, fieldSet, index++);
     assertEquals(FIELD_COUNT, index);
@@ -53,11 +55,16 @@ public class ImdbMovieLineTokenizerTest {
   @Test
   public void test_single_movie_episode_date() {
     FieldSet fieldSet = lineTokenizer.tokenize("\"+ Clair\" (2001) {(2004-09-04)}                         2004");
-    assertEquals(FIELD_COUNT, fieldSet.getFieldCount());
     assertFiedEquals("+ Clair", fieldSet, index++);
     assertFiedEquals("2001", fieldSet, index++);
     assertFiedEquals(null, fieldSet, index++);
+    assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals("2004-09-04", fieldSet, index++);
+    assertFiedEquals(null, fieldSet, index++);
+    assertFiedEquals(null, fieldSet, index++);
+    assertFiedEquals(null, fieldSet, index++);
+    assertFiedEquals(null, fieldSet, index++);
+    assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals("2004", fieldSet, index++);
@@ -68,11 +75,13 @@ public class ImdbMovieLineTokenizerTest {
   @Test
   public void test_single_movie_episode_name() {
     FieldSet fieldSet = lineTokenizer.tokenize("\"(S)truth\" (2003) {Metric Time}                         2003");
-    assertEquals(FIELD_COUNT, fieldSet.getFieldCount());
     assertFiedEquals("(S)truth", fieldSet, index++);
     assertFiedEquals("2003", fieldSet, index++);
     assertFiedEquals(null, fieldSet, index++);
+    assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals("Metric Time", fieldSet, index++);
+    assertFiedEquals(null, fieldSet, index++);
+    assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals("2003", fieldSet, index++);
@@ -83,11 +92,11 @@ public class ImdbMovieLineTokenizerTest {
   @Test
   public void test_tv_movie() {
     FieldSet fieldSet = lineTokenizer.tokenize("\"A Punt, a Pass, and a Prayer\" (1968) (TV)\t\t\t\t1968");
-    assertEquals(FIELD_COUNT, fieldSet.getFieldCount());
     assertFiedEquals("A Punt, a Pass, and a Prayer", fieldSet, index++);
     assertFiedEquals("1968", fieldSet, index++);
     assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals("TV", fieldSet, index++);
+    assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals(null, fieldSet, index++);
@@ -100,9 +109,9 @@ public class ImdbMovieLineTokenizerTest {
   @Test
   public void test_the_terminator() {
     FieldSet fieldSet = lineTokenizer.tokenize("\"The Terminator\" (1984)\t\t\t\t\t1984");
-    assertEquals(FIELD_COUNT, fieldSet.getFieldCount());
     assertFiedEquals("The Terminator", fieldSet, index++);
     assertFiedEquals("1984", fieldSet, index++);
+    assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals(null, fieldSet, index++);
@@ -117,13 +126,14 @@ public class ImdbMovieLineTokenizerTest {
   @Test
   public void test_movie_suspended() {
     FieldSet fieldSet = lineTokenizer.tokenize("\"'Til Death\" (2006) {No Complaints (#3.14)} {{SUSPENDED}}       ????");
-    assertEquals(FIELD_COUNT, fieldSet.getFieldCount());
     assertFiedEquals("'Til Death", fieldSet, index++);
     assertFiedEquals("2006", fieldSet, index++);
+    assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals("No Complaints", fieldSet, index++);
     assertFiedEquals("3", fieldSet, index++);
     assertFiedEquals("14", fieldSet, index++);
+    assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals("SUSPENDED", fieldSet, index++);
     assertFiedEquals("????", fieldSet, index++);
     assertFiedEquals(null, fieldSet, index++);
@@ -133,11 +143,12 @@ public class ImdbMovieLineTokenizerTest {
   @Test
   public void test_movie_episodes_suspended() {
     FieldSet fieldSet = lineTokenizer.tokenize("\"Action Man\" (2000) {Various episodes} {{SUSPENDED}}	????");
-    assertEquals(FIELD_COUNT, fieldSet.getFieldCount());
-    assertFiedEquals("Action Manh", fieldSet, index++);
+    assertFiedEquals("Action Man", fieldSet, index++);
     assertFiedEquals("2000", fieldSet, index++);
+    assertFiedEquals(null, fieldSet, index++);
+    assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals("Various episodes", fieldSet, index++);
-    assertFiedEquals("SUSPENDED", fieldSet, index++);
+    assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals("SUSPENDED", fieldSet, index++);
@@ -149,10 +160,10 @@ public class ImdbMovieLineTokenizerTest {
   @Test
   public void test_movie_episodes_romain() {
     FieldSet fieldSet = lineTokenizer.tokenize("\"Wanted\" (2011/IV)\t\t\t\t2011");
-    assertEquals(FIELD_COUNT, fieldSet.getFieldCount());
     assertFiedEquals("Wanted", fieldSet, index++);
     assertFiedEquals("2011", fieldSet, index++);
     assertFiedEquals("IV", fieldSet, index++);
+    assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals(null, fieldSet, index++);
     assertFiedEquals(null, fieldSet, index++);
