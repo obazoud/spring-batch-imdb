b794eec9ff72073b6b9684c9446f37191862d342
Olivier Bazoud <olivier.bazoud@gmail.com>
2014-02-12 10:42:41 +0100

    regexp wip

--- imdb-data/src/main/java/com/bazoud/springbatch/imdb/data/reader/ImdbMovieLineTokenizer.java
+++ imdb-data/src/main/java/com/bazoud/springbatch/imdb/data/reader/ImdbMovieLineTokenizer.java
@@ -7,13 +7,13 @@ import org.springframework.beans.factory.InitializingBean;
 
 public class ImdbMovieLineTokenizer extends RegexLineTokenizer implements InitializingBean {
   private static final Logger LOG = LoggerFactory.getLogger(ImdbMovieLineTokenizer.class);
+
   private String MOVIE_NAME_PATTERN = "\"(.+?)\"";
 
   private String RELEASE_DATE_PATTERN = "\\s+\\((?:(\\d{4}||\\?{4}))(?:\\/([IXV]+))?\\)";
 
   private String TV_PATTERN = "(?:\\s+\\(((?:V)||(?:TV)||(?:VG))\\))?";
 
-  // private String EPISODE_PATTERN = "(?:\\s+\\{(.+?)?(?:\\s+\\((?:#(?:(\\d+?)\\.(\\d+?)))?(\\d{4}-\\d{2}-\\d{2})?\\))?\\})?";
   private String EPISODE_PATTERN = "(?:\\s+\\{([^(]+?)?\\s*(?:(?:\\(#(?:(\\d*)\\.(\\d*)\\)))?(?:(?:\\((\\d{4}-\\d{2}-\\d{2})\\)))?)?\\})?(?:\\s+\\{\\{(.+?)?\\}\\})?";
 
   private String MOVIE_STATE = "(?:\\s+\\{\\{(.+?)?\\}\\})?";
